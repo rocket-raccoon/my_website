caaf8d3ffabe03dc9317b8f8717f42ea2b62b188
abenavides <abenavides@shutterstock.com>
2016-05-11 21:06:11 -0400

    Creating minimal as fuck express server

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
 // Dependencies
-var http = require('http');
+var express = require('express');
 
+// Configure our app
+var app = express();
 var port = 3000;
-var run_server = function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World, this is going to be my personal website!');
+app.set('port', process.env.PORT || port);
+
+// Create a home page
+var pageHome = function(req, res) {
+    res.type('text/plain');
+    res.send('The Tristanity Home Page');
 };
-http.createServer(run_server).listen(port);
-console.log("Server started on localhost: " + port);
+app.get('/', pageHome);
+
+// Create an about page
+var pageAbout = function(req, res) {
+    res.type('text/plain');
+    res.send('This site is a personal website for me Alberto Tristan Benavides');
+};
+app.get('/about', pageAbout);
+
+// Create 404 page
+var page404 = function(req, res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Page Not Found');
+};
+app.use(page404);
+
+// Create 500 page
+var page500 = function(err, req, res, next) {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+};
+app.use(page500);
+
+// Run the local server
+var run_server = function() {
+    console.log('Express started on http://localhost:' + app.get('port'));
+};
+app.listen(app.get('port'), run_server);
