d19a561e39088695391b99e1386ac7c3d532438f
abenavides <abenavides@shutterstock.com>
2016-05-11 21:16:07 -0400

    Adding handlebars to my site

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 // Dependencies
-var express = require('express');
+var express    = require('express');
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main'});
 
 // Configure our app
 var app = express();
 var port = 3000;
 app.set('port', process.env.PORT || port);
 
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
+
 // Create a home page
 var pageHome = function(req, res) {
-    res.type('text/plain');
-    res.send('The Tristanity Home Page');
+    res.render('home');
 };
 app.get('/', pageHome);
 
 // Create an about page
 var pageAbout = function(req, res) {
-    res.type('text/plain');
-    res.send('This site is a personal website for me Alberto Tristan Benavides');
+    res.render('about');
 };
 app.get('/about', pageAbout);
 
 // Create 404 page
 var page404 = function(req, res) {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Page Not Found');
+    res.render('404');
 };
 app.use(page404);
 
 // Create 500 page
 var page500 = function(err, req, res, next) {
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 };
 app.use(page500);
 
